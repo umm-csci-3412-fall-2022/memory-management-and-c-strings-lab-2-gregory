2e03d1301f222bf0a85c6b3e3e4a143108b2244b
Gregory Peterson <pet03356@morris.umn.edu>
2022-11-08 16:22:11 -0600

    Worked on disemvowel.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -2,6 +2,21 @@
 
 #include "disemvowel.h"
 
+char* vowels = "aeiouAEIOU";
+int isvowel(char c){
+	for (i=0; j<11; ++j){
+	       if	
+
 char *disemvowel(char *str) {
+	int len, i;
+	char *vowels;
+
+	len = strlen(str);
+	vowels = (char*) calloc(len+1, sizeof(char));
+
+	for (i=0; i<len; ++i){
+		{
+			if(str[i] == vowels[j])
+
   return (char*) "";
 }
